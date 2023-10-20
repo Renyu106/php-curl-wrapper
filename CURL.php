fb50d747e8cc66b23f1d3bf29736af8cc6c0a7cf
Renyu <admin@winsub.kr>
2023-10-20 16:24:23 +0000

    어휴 조잡해라

--- CURL.php
+++ CURL.php
@@ -1,46 +1,51 @@
 <?php
 
-class CURL
-{
+class CURL {
     private $url;
     private $method;
     private $HEDAERS;
     private $postData;
     private $PROXY;
+    private $REDIRECT_ALLOW = false;
 
     private $PROXYS = array(
-        "localhost:12345"
+        "127.0.0.1:1234"
     );
 
-    public function __construct($url, $method = 'GET', $HEDAERS = [])
-    {
+    public function __construct($url, $method = 'GET') {
         $this->url = $url;
         $this->method = $method;
-        $this->HEDAERS = $HEDAERS;
     }
 
-    public function DATA($data)
-    {
+    public function DATA($data) {
         $this->postData = $data;
     }
 
-    public function PROXY($PROXY)
-    {
+    public function GET_DATA() {
+        return $this->postData;
+    }
+
+    public function GET_PROXY() {
+        return $this->PROXY;
+    }
+
+    public function PROXY($PROXY) {
         $this->PROXY = $PROXY;
     }
 
-    public function RANDOM_PROXY()
-    {
+    public function RANDOM_PROXY() {
         $this->PROXY = $this->PROXYS[array_rand($this->PROXYS)];
     }
 
-    public function HAEDER($HEDAERS)
-    {
+    public function HAEDER($HEDAERS) {
         $this->HEDAERS = $HEDAERS;
     }
 
-    public function SEND()
-    {
+    public function REDIRECT_ALLOW(){
+        $this->REDIRECT_ALLOW = true;
+    }
+
+    public function SEND() {
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $this->url);
@@ -51,7 +56,7 @@ class CURL
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
         if ($this->HEDAERS) {
-            $this->HEDAERS[] = 'User-Agent: WinSub-CURL';
+            $this->HEDAERS[] = 'User-Agent: PHP-CURL';
             curl_setopt($ch, CURLOPT_HTTPHEADER, $this->HEDAERS);
         }
 
@@ -63,17 +68,30 @@ class CURL
             curl_setopt($ch, CURLOPT_PROXY, $this->PROXY);
         }
 
-        $response = curl_exec($ch);
-        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $header = substr($response, 0, $header_size);
-        $body = substr($response, $header_size);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+        if($this->REDIRECT_ALLOW){
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        }
+
+        try{
+            $response = curl_exec($ch);
+            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $header = substr($response, 0, $header_size);
+            $body = substr($response, $header_size);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $status = "OK";
+            curl_close($ch);
+        } catch (\Exception $e) {
+            $header = null;
+            $body = $e;
+            $httpCode = null;
+            $status = "ERR";
+        }
 
         return [
-            'HEADER' => $header,
-            'BODY' => $body,
-            'CODE' => $httpCode
+            "STATUS" => $status,
+            "HEADER" => $header,
+            "BODY" => $body,
+            "CODE" => $httpCode,
         ];
     }
 }
